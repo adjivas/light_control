2a96e8c4495ddfc2a75ad729682bd3f624d9048d
navi <navi@42.fr>
2023-05-06 02:06:13 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
+mod light;
 mod request;
 
 use std::error::Error;
 use std::iter;
 use std::{time::{Duration, Instant}, thread::sleep};
 
+use light::Light;
+
 use itertools::Itertools;
 use itertools::FoldWhile::{Continue, Done};
-use request::Light;
 use rumqttc::v5::{Client, MqttOptions, Event};
 use rumqttc::v5::mqttbytes::QoS;
 use rumqttc::v5::mqttbytes::v5::{Publish, Packet};
+use chrono::{Local, Datelike};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let http_rest_host = Box::leak(std::env::var("HTTP_REST_HOST").unwrap().into_boxed_str()) as &'static str;
+    let http_rest_pass = Box::leak(std::env::var("HTTP_REST_PASS").unwrap().into_boxed_str()) as &'static str;
     let mqtt_name = Box::leak(std::env::var("MQTT_NAME").unwrap().into_boxed_str()) as &'static str;
     let mqtt_host = Box::leak(std::env::var("MQTT_HOST").unwrap().into_boxed_str()) as &'static str;
     let mqtt_port = Box::leak(std::env::var("MQTT_PORT").unwrap().into_boxed_str()) as &'static str;
@@ -19,10 +24,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mqtt_subcribe = Box::leak(std::env::var("MQTT_SUBSCRIBE").unwrap().into_boxed_str()) as &'static str;
     let mqtt_keep_alive = Duration::from_secs(10);
     let mqtt_interval = Duration::from_millis(100);
-    let http_rest_host = Box::leak(std::env::var("HTTP_REST_HOST").unwrap().into_boxed_str()) as &'static str;
-    let http_rest_pass = Box::leak(std::env::var("HTTP_REST_PASS").unwrap().into_boxed_str()) as &'static str;
-    let switch_exposure = Duration::from_secs(5);
-    let switch_post_exposure = 30;
+    let light_exposure = Box::leak(std::env::var("LIGHT_EXPOSURE").unwrap().into_boxed_str()) as &'static str;
+    let light_exposure = Duration::from_secs(light_exposure.parse::<u64>().unwrap());
+    let light_post_exposure = Box::leak(std::env::var("LIGHT_POST_EXPOSURE").unwrap().into_boxed_str()) as &'static str;
+    let light_post_exposure = light_post_exposure.parse::<usize>().unwrap();
+    let light_latitude = Box::leak(std::env::var("LIGHT_LATITUDE").unwrap().into_boxed_str()) as &'static str;
+    let light_latitude = light_latitude.parse::<f64>().unwrap();
+    let light_longitude = Box::leak(std::env::var("LIGHT_LONGITUDE").unwrap().into_boxed_str()) as &'static str;
+    let light_longitude = light_longitude.parse::<f64>().unwrap();
 
     let mut mqtt_options = MqttOptions::new(mqtt_name, mqtt_host, mqtt_port);
     mqtt_options.set_keep_alive(mqtt_keep_alive);
@@ -38,22 +47,28 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
     loop {
+        let local = Local::now();
+        let (sunrise, sunset) = sunrise::sunrise_sunset(light_latitude, light_longitude, local.year(), local.month(), local.day());
+        let sunshine: bool = sunrise <= local.timestamp() && local.timestamp() <= sunset;
+
         if let Some(Some(was_time)) = it_events.next() {
-            if Light::power_on(&http_rest_host, http_rest_pass).is_some() {
-                println!("PowerOn!{:?}", was_time);
+            if !sunshine {
+                if Light::power_on(&http_rest_host, http_rest_pass).is_some() {
+                    println!("PowerOn!{:?}", was_time);
 
-                it_events.fold_while(was_time, |was_time, event| match event {
-                    Some(time) => Continue(time),
-                    None if was_time + switch_exposure > Instant::now() => Continue(was_time),
-                    None => Done(was_time),
-                }).into_inner();
+                    it_events.fold_while(was_time, |was_time, event| match event {
+                        Some(time) => Continue(time),
+                        None if was_time + light_exposure > Instant::now() => Continue(was_time),
+                        None => Done(was_time),
+                    }).into_inner();
 
-                println!("PowerOff!{:?}", Instant::now());
+                    println!("PowerOff!{:?}", Instant::now());
 
-                if Light::power_off(&http_rest_host, http_rest_pass).is_some() {
-                    it_events.dropping(switch_post_exposure);
+                    if Light::power_off(&http_rest_host, http_rest_pass).is_some() {
+                        it_events.dropping(light_post_exposure);
 
-                    println!("PowerPostOff!{:?}", Instant::now());
+                        println!("PowerPostOff!{:?}", Instant::now());
+                    }
                 }
             }
         }
--- src/request.rs
+++ src/request.rs
@@ -1,27 +1,6 @@
+use super::Light;
+
 use std::error::Error;
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Default, Eq, PartialEq)]
-pub enum State {
-    #[default]
-    ON,
-    OFF,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Default)]
-struct Color {
-    r: u8,
-    g: u8,
-    b: u8,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, Default)]
-pub struct Light {
-    pub state: State,
-    brightness: u8,
-    white_value: u8,
-    color: Color,
-}
 
 pub fn get(http_rest_host: &str, http_rest_pass: &str) -> Result<Light, Box<dyn Error>> {
     let client: reqwest::blocking::Client = reqwest::blocking::Client::new();
@@ -39,37 +18,4 @@ pub fn patch(http_rest_host: &str, http_rest_pass: &str, message: String) -> Res
         .body(message).send()?;
 
     Ok(request.json()?)
-}
-
-impl Light {
-    pub fn is_powered_off(http_rest_host: &str, http_rest_pass: &str) -> Result<bool, Box<dyn Error>>  {
-        match get(http_rest_host, http_rest_pass) {
-            Ok(Light { state, .. }) => Ok(state == State::OFF),
-            Err(why) => Err(why),
-        }
-    }
-
-    pub fn power_on(http_rest_host: &str, http_rest_pass: &str) -> Option<Light> {
-        match Self::is_powered_off(http_rest_host, http_rest_pass) {
-            Ok(true) => {
-                let ref light: Light = Light {state: State::ON, brightness: u8::MAX, white_value: u8::MAX, ..Default::default()};
-                let message = serde_json::to_string(light).ok()?;
-
-                patch(http_rest_host, http_rest_pass, message).ok()
-            },
-            _ => None,
-        }
-    }
-
-    pub fn power_off(http_rest_host: &str, http_rest_pass: &str) -> Option<Light> {
-        match Self::is_powered_off(http_rest_host, http_rest_pass) {
-            Ok(false) => {
-                let ref light: Light = Light {state: State::OFF, ..Default::default()};
-
-                let message = serde_json::to_string(light).ok()?;
-                patch(http_rest_host, http_rest_pass, message).ok()
-            },
-            _ => None,
-        }
-    }
 }
